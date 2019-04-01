3b077130f77ec58c9db0e8056c798b4cb8e724fb
sherman <sherman@sherman.com>
2019-04-01 21:46:06 +0900

    Added E_1_2.c by Sherman HUO on 4/1/2019

---
+++ E_1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+main()
+{
+    printf("hello, world\n");
+    printf("hello, ");
+    printf("\102"); //Octal number B
+    printf("\x42"); //Hexadecimal number B
+    printf("\c ");
+    printf("world");
+    printf("\n");
+}

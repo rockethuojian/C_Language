0663988a338dca62c793dcef957b2783459a11cc
sherman <sherman@sherman.com>
2019-04-10 23:09:54 +0900

    Added E_1_9.c by S.H. on 4/10/2019

---
+++ E_1_9.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+main()
+{
+    int c, blank_flag;
+
+    blank_flag = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+            blank_flag = 0;
+        else
+        {
+            if (blank_flag != 0)
+            {
+                putchar(' ');
+                putchar(c);
+            }
+            else
+                putchar(c);
+        }
+
+
+
+
+#if 0
+        if (blank_flag == 0)
+            if (c == ' ')
+                blank_flag = 1;
+            else 
+                putchar(c);
+        else 
+            if (c != ' ')
+            {
+            putchar(' ');
+            blank_flag = 0;
+            }
+#endif
+    }
+}

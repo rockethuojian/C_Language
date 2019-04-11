19bde08d1d2f03db2d87cef020f13300e24a5a8c
sherman <sherman@sherman.com>
2019-04-11 23:11:34 +0900

    Deletede unnecessary code by S.H. on 4/11/2019

--- E_1_10.c
+++ E_1_10.c
@@ -16,57 +16,5 @@ main()
             printf("\\\\");
         else
             putchar(c);
-
-
-
-
-
-
-#if 0
-
-        if (c != ' ')
-        {
-            putchar(c);
-            blank_flag = 0;
-        }
-        else
-            if (blank_flag == 0)
-            {
-                putchar(' ');
-                blank_flag = 1;
-            }
-
-
-
-
-        if (c == ' ')
-            blank_flag = 1;
-        else
-        {
-            if (blank_flag != 0)
-            {
-                putchar(' ');
-                putchar(c);
-                blank_flag = 0;
-            }
-            else
-                putchar(c);
-        }
-
-
-
-
-        if (blank_flag == 0)
-            if (c == ' ')
-                blank_flag = 1;
-            else 
-                putchar(c);
-        else 
-            if (c != ' ')
-            {
-            putchar(' ');
-            blank_flag = 0;
-            }
-#endif
     }
 }
